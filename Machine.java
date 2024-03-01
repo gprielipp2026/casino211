0a1032810c4df43f961dbd97a5ee0c2eff646a44
gprielipp2026 <m265112@usna.edu>
2024-03-01 11:05:20 -0500

    Machine

--- Machine.java
+++ Machine.java
@@ -1,7 +1,71 @@
+import java.util.*; // for Random
+
 public class Machine
 {
-  public double play() { return 1.25; }
-  public boolean inService() { return true; }
-  public void report() { System.out.println("Machine report"); }
-  public String getName() { return "Machine"; }
+  private String name;
+  protected double maxPayout;
+  private double potCasino;
+  private double potAvailable;
+  private double initialAvailable
+  private boolean putInCasinoPot;
+  protected static final Random rand = new Random(System.currentTimeMillis());
+
+  public Machine(String name, double maxPayout, double potAvailable)
+  {
+    this.name = name;
+    this.maxPayout = maxPayout;
+    this.potCasino = 0;
+    this.potAvailable = potAvailable;
+    this.initialAvailable = potAvailable;
+    this.putInCasinoPot = true;
+  }
+
+  // where implementation of the machine really happens
+  // catch phrase before implementation
+  protected boolean playerWins()
+  {
+    System.out.println(getName() + ": You win! --> $" + maxPayout);
+    return true;
+  }
+
+  public double play() 
+  { 
+    // each play takes one quarter ($0.25)
+    // 1st quarter goes to the Casino
+    // 2nd quarter goes to the Available
+    if(putInCasinoPot)
+    {
+      potCasino += 0.25;
+    }
+    else
+    {
+      potAvailable += 0.25;
+    }
+    putInCasinoPot = !putInCasinoPot;
+
+    // perform the play
+    if(inService() && playerWins())
+    {
+      potAvailable -= maxPayout;
+
+      return maxPayout; 
+    }
+    return -0.25; // did not win (the machine took the money)
+  }
+  
+  public boolean inService() { return potAvailable > maxPayout; }
+  
+  public void report() 
+  { 
+    // report potCasino = "casino"
+    System.out.print("casino: $" + potCasino + ", "); 
+    
+    // report potAvailable = "pot"
+    System.out.print("pot: $" + potAvailable + ", "); 
+    
+    // report potCasino + potAvailable = "net"
+    System.out.println("net: $" + (potCasino + potAvailable - initialAvailable) ); 
+  }
+
+  public String getName() { return name; }
 }
