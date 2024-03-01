924765e9d5c268e5d90c8e4a5f1a6b7b125afe5d
gprielipp2026 <m265112@usna.edu>
2024-03-01 11:14:31 -0500

    Lab08

---
+++ Lab08.java
@@ -0,0 +1,32 @@
+public Lab08
+{
+  public static void main(String[] args)
+  {
+    if(args.length < 2)
+    {
+      System.out.println("usage java Lab08 <initWallet> <setupFile>");
+      System.exit(1);
+    }
+
+    double wallet = String.parseDouble(args[0]);
+    String setupFN = args[1];
+
+    Machine[] machines = Reader.machines(setupFN);
+    Manager manager = new Manager(machines);
+
+    Scanner in = new Scanner(System.in);
+    String cmd;
+    
+    do
+    {
+      manager.draw();
+
+      System.out.print("> ");
+      
+      cmd = in.next(); 
+      manager.exec(cmd); // if manager doesn't recognize the cmd, 
+                         // then it does nothing
+                         // so it should do nothing on "quit"
+    } while( !cmd.equals("quit") );
+  }
+}
