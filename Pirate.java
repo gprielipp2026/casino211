8306eb917d698e8a0ff599bb995ab4c3c0b4f649
NSchmidt48067 <m265646@usna.edu>
2024-03-01 11:44:07 -0500

    sdf

--- Pirate.java
+++ Pirate.java
@@ -1,5 +1,35 @@
 public class Pirate extends Machine
 {
+    public Pirate(String n, double i, double o){
+        super(n, o, i);
+    }
 
+    protected boolean playerWins(){
+        char[] vals = new char[3];
+        int num;
 
+        for (int i = 0; i < 3; i++){
+        num = rand.nextInt(3);
+        if (num == 0){
+            vals[i] = '♈';
+        }
+        else if (num == 1){
+            vals[i] = '✋';
+        }
+        else {
+            vals[i] = '⛵';
+        }
+        }
+    
+
+    if (vals[0] == vals[1] && vals[0] == vals[2]) {
+        System.out.println("Machine: " + vals[0] + vals[1] + vals[2] + " You've won and taken me treasure, scaliwag! --> " + maxPayout);
+        return true;
+    }
+    else {
+        System.out.println("Machine: " + vals[0] + vals[1] + vals[2] + " Arr, you lost, matey!");
+        return false;
+    }
+
+}
 }
--- Reader.java
+++ Reader.java
@@ -1,8 +1,47 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
 public class Reader
 {
   public static Machine[] machines(String fname)
   {
-    
-    return null;
+    //Scanner for file
+    Scanner sc = null;
+    try { sc = new Scanner(new FileReader(fname)); } 
+    catch(IOException e) { e.printStackTrace(); System.exit(1); }
+
+    String len;
+    len = sc.next();
+    int counter = len.length();
+
+    Machine[] m = new Machine[counter];
+
+    Machine tmp = null;
+    String name;
+    double p, in, pay;
+
+    //Loop to create array of machines
+    for (int i = 0; i < counter; i++){
+      name = sc.next();
+
+      if (name == "Yoda") {
+        p = sc.nextDouble();
+        in = sc.nextDouble();
+        pay = sc.nextDouble();
+        tmp = new Yoda(name, p, in, pay);
+      }
+      else if (name == "Pirate"){
+        in = sc.nextDouble();
+        pay = sc.nextDouble();
+        tmp = new Pirate(name, in, pay);
+      }
+
+      m[counter] = tmp;
+
+    }
+
+
+    return m;
   }
 }
